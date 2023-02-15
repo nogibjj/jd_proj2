17d50bd3c4e55c6f15bc320d43b0a62fea078607
Jackie Du <86393045+jacdu@users.noreply.github.com>
2023-02-15 00:38:10 +0000

    initial changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn welcome() -> impl Responder {
+    HttpResponse::Ok().body("Hello!")
+}
+
+#[get("/test")]
+async fn test() -> impl Responder {
+    HttpResponse::Ok().body("This is a test")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    //add a print message to the console that the service is running
+    println!("Starting service...");
+    HttpServer::new(|| {
+        App::new()
+            .service(welcome)
+            .service(test)
+
+    })
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
 }
