ea7e3b86696a9f6a402bcfcbc6237280ab6f2539
Jackie Du <86393045+jacdu@users.noreply.github.com>
2023-02-21 16:53:19 +0000

    try cpython

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,45 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+// use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
-#[get("/")]
-async fn welcome() -> impl Responder {
-    HttpResponse::Ok().body("Hello!")
+// #[get("/")]
+// async fn welcome() -> impl Responder {
+//     HttpResponse::Ok().body("Hello!")
+// }
+
+// #[get("/test")]
+// async fn test() -> impl Responder {
+//     HttpResponse::Ok().body("This is a test")
+// }
+
+// #[actix_web::main]
+// async fn main() -> std::io::Result<()> {
+//     //add a print message to the console that the service is running
+//     println!("Starting service...");
+//     HttpServer::new(|| {
+//         App::new()
+//             .service(welcome)
+//             .service(test)
+
+//     })
+//         .bind("0.0.0.0:8080")?
+//         .run()
+//         .await
+// }
+
+use cpython::{Python, PyDict, PyResult};
+
+fn main() {
+    let gil = Python::acquire_gil();
+    hello(gil.python()).unwrap();
 }
 
-#[get("/test")]
-async fn test() -> impl Responder {
-    HttpResponse::Ok().body("This is a test")
-}
+fn hello(py: Python) -> PyResult<()> {
+    let sys = py.import("sys")?;
+    let version: String = sys.get(py, "version")?.extract(py)?;
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    //add a print message to the console that the service is running
-    println!("Starting service...");
-    HttpServer::new(|| {
-        App::new()
-            .service(welcome)
-            .service(test)
+    let locals = PyDict::new(py);
+    locals.set_item(py, "os", py.import("os")?)?;
+    let user: String = py.eval("os.getenv('USER') or os.getenv('USERNAME')", None, Some(&locals))?.extract(py)?;
 
-    })
-        .bind("0.0.0.0:8080")?
-        .run()
-        .await
+    println!("Hello {user}, I'm Python {version}");
+    Ok(())
 }
